7cfe8697ff51677223425593b6b0e859f2c586bc
lukitss55 <lucascolman215@gmail.com>
2026-04-09 20:43:03 -0300

    Update prueba.blade.php

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=@, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <title>Document</title>
 </head>
 <body>
+
     <div class="container">
         @include('nav')
         
     </div>
+
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
 </body>
 </html>
