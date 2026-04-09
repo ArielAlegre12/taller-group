11e8dc9eac0ed80716812cddc5df5879c1386ced
lukitss55 <lucascolman215@gmail.com>
2026-04-09 18:35:43 -0300

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,3 +1,37 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid"></div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Huellas Felices</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#huellas-felices"
+            aria-controls="huellas-felices" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="huellas-felices">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Principal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tienda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Servicios</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">Quienes somos</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Información de contactos</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Términos y Uso</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Consultas</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+        </div>
+    </div>
 </nav>
