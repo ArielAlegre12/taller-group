fd32548b0e5f168a6082bec11c0ab8aabcf10202
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-04-10 18:20:08 -0300

    agregué @vite, para cuando estámos codeando ver los cambios en vivo sin tener que recargar la pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','Huellas Felices')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     @stack('styles')
+    
 </head>
 <body>
     @include('partials.nav')
--- resources/views/pages/tienda.blade.php
+++ resources/views/pages/tienda.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="{{ asset('css/tiendaStyle.css') }}">
 @endpush
 @section('title')
-Tienda
+    Tienda
 @endsection
 @section('content')
     <div class="contenedor-categoria">
@@ -19,24 +19,30 @@ Tienda
                 </div>
                 <div>
                     <h5>Higene y Cuidado de la Mascota</h5>
-                    <li>Champús y acondicionadores</li>
-                    <li>Antipulgas y desparasitantes</li>
-                    <li>cuidado oral y limpieza</li>
-                    <li>Arena para gatos</li>
+                    <ul>
+                        <li>Champús y acondicionadores</li>
+                        <li>Antipulgas y desparasitantes</li>
+                        <li>cuidado oral y limpieza</li>
+                        <li>Arena para gatos</li>
+                    </ul>
                 </div>
                 <div>
                     <h5>Accesorios y Accesorios de Paseo</h5>
-                    <li>Correas, collares y pecheras</li>
-                    <li>Juguetes</li>
-                    <li>Camas y mantas</li>
-                    <li>Transportadoras y bolsos</li>
-                    <li>Comederos y bebederos</li>
+                    <ul>
+                        <li>Correas, collares y pecheras</li>
+                        <li>Juguetes</li>
+                        <li>Camas y mantas</li>
+                        <li>Transportadoras y bolsos</li>
+                        <li>Comederos y bebederos</li>
+                    </ul>
                 </div>
                 <div>
                     <h5>Productos de Farmacia(Venta Libre)</h5>
-                    <li>Collares isabelinos</li>
-                    <li>Lociones, perfumes y productos desenrededantes</li>
-                    <li>productos de higene general</li>
+                    <ul>
+                        <li>Collares isabelinos</li>
+                        <li>Lociones, perfumes y productos desenrededantes</li>
+                        <li>productos de higene general</li>
+                    </ul>
                 </div>
             </div>
     </div>
