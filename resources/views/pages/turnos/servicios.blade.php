575b8d8cb596510af5add8b81a11e0512dbdde44
lukitss55 <lucascolman215@gmail.com>
2026-04-12 01:44:38 -0300

    actualizacion
    modifique los botones para que cuando se minimice o se vea del celu se vea bien

--- resources/views/pages/turnos/servicios.blade.php
+++ resources/views/pages/turnos/servicios.blade.php
@@ -1,32 +1,36 @@
 @extends('layouts.app')
+
 @push('styles')
     <link rel="stylesheet" href="{{ asset('css/turnos.css') }}">
 @endpush
+
 @section('title')
     Servicios de turnos
 @endsection
+
 @section('content')
+
     <div class="header-turno">
         <div class="container">
             <h1 class="text-center">Servicios de turnos</h1>
         </div>
     </div>
-    <div class="container mt-5">
-        <div class="container d-flex justify-content-center align-items-center" style="height: 80vh;">
-            <div class="row text-center">
-                <div class="col-md-6 mb-4">
-                    <a href="{{ route('pages.turnos.produccion') }}" class="btn btn-outline-success custom-btn">
-                        <img src="{{ asset('images/caballo.png') }}" alt="caballo-cabeza">
-                        <p>Animales de produccion</p>
-                    </a>
-                </div>
-                <div class="col-md-6 mb-4">
-                    <a href="{{ route('pages.turnos.domestico') }}" class="btn btn-outline-success custom-btn">
-                        <img src="{{ asset('images/gato.png') }}" alt="gato-cabeza">
-                        <p>Animales domesticos</p>
-                    </a>
-                </div>
+    <div class="container-fluid mt-5">
+        <div class="row text-center ">
+            <div class="col-12 col-md-6 mb-2">
+                <a href="{{ route('pages.turnos.produccion') }}" class="btn btn-outline-success custom-btn">
+                    <img src="{{ asset('images/caballo.png') }}" alt="caballo">
+                    <p>Animales de producción</p>
+                </a>
             </div>
+            <div class="col-12 col-md-6 mb-2">
+                <a href="{{ route('pages.turnos.domestico') }}" class="btn btn-outline-success custom-btn">
+                    <img src="{{ asset('images/gato.png') }}" alt="gato">
+                    <p>Animales domésticos</p>
+                </a>
+            </div>
+
         </div>
     </div>
+
 @endsection
