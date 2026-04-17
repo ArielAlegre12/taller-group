e0c2fd562c6aa094d3ff0fd70f53f003267e74ba
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-04-17 17:07:35 -0300

    cambie-nav-y-agregué-img-inicio

--- resources/views/pages/principal.blade.php
+++ resources/views/pages/principal.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
-
+    <img src="https://images.unsplash.com/photo-1770836037793-95bdbf190f71?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx2ZXRlcmluYXJpYW4lMjBleGFtaW5pbmclMjBkb2d8ZW58MXx8fHwxNzc2MzI5NDc0fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+        alt="" class="w-100 h-100 object-cover">
 
 @endsection
